2bce9e9c85a3614ca0a5a8acd2f6285542d79e1f
gcorbel <guirec.corbel@gmail.com>
2013-03-08 10:52:27 -0500

    add basics for devise

---
+++ db/migrate/20130308154332_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :github,         default: ""
+      t.string :twitter,        default: ""
+      t.string :stackoverflow,  default: ""
+      t.boolean :email_visible, default: true
+      t.string :website,        default: ""
+      t.string :username,       default: "", null: false
+      t.string :street,         default: ""
+      t.string :city,           default: ""
+      t.string :postal_code,    default: ""
+
+      t.timestamps
+    end
+  end
+end

7936f5d40333bc7cb90bfcffbf0919fa3de8f39a
gcorbel <guirec.corbel@gmail.com>
2013-03-08 16:06:36 -0500

    add a list of members

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       content_tag :div, button_tag + message_tag , class: "alert alert-#{class_name}"
     end
   end
+
+  def avatar_url(user, size=100)
+    default_url = "http://#{request.host}:#{request.port}/assets/guest.png"
+    return default_url if user.nil? || !Rails.env.production?
+    avatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{avatar_id}.png" \
+      "?s=#{size}&d=#{CGI.escape(default_url)}"
+  end
+
+  def image_for(user)
+    image_tag(avatar_url(user, 76), class: 'avatar')
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
+  let(:user) { build_stubbed(:user) }
+  let(:avatar_id) { Digest::MD5::hexdigest(user.email).downcase }
+  let(:default_url) { "http://test.host:80/assets/guest.png"}
+
   describe :alert_box do
     context "when there is a message" do
       subject { helper.alert_box("info", "message") }
@@ -14,6 +18,28 @@ describe ApplicationHelper do
       subject { helper.alert_box("info", nil) }
 
       it { should be_nil }
+    end
+  end
+
+  describe :avatar_url do
+    before { Rails.env.stubs(:test?).returns(false) }
+    after { Rails.env.stubs(:test?).returns(true) }
+
+    it "give a path to the avatar" do
+      expect(helper.avatar_url(user)).to eq "http://gravatar.com/avatar/#{avatar_id}.png?s=100&d=#{CGI.escape(default_url)}"
+      Rails.env.stubs(:test?).returns(true)
+    end
+
+    context "when a size is specified" do
+      it "give a path to the avatar with a size" do
+        expect(helper.avatar_url(user, 50)).to eq "http://gravatar.com/avatar/#{avatar_id}.png?s=50&d=#{CGI.escape(default_url)}"
+      end
+    end
+
+    context "the user is nil" do
+      it "give a path to the avatar with a size" do
+        expect(helper.avatar_url(nil)).to eq "http://test.host:80/assets/guest.png"
+      end
     end
   end
 end

66f9088e315e8c34d5bf7ec6021c378062c53939
gcorbel <guirec.corbel@gmail.com>
2013-03-09 11:30:51 -0500

    change the factory to pass validations

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     f.password "test1234"
     f.password_confirmation "test1234"
     f.sequence(:email){ |n| "guirec.corbel#{n}@gmail.com"}
+    postal_code "A1B2C3"
   end
 end

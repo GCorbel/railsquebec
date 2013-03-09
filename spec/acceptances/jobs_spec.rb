56c04296aa022496539907239afa78c43ccfe45a
gcorbel <guirec.corbel@gmail.com>
2013-03-09 15:26:26 -0500

    add a page to show a job

--- spec/acceptances/jobs_spec.rb
+++ spec/acceptances/jobs_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 feature "jobs" do
+  let!(:job) { create(:job) }
+
   scenario "Show the list of jobs" do
-    job = create(:job)
     visit jobs_path
+    expect(page).to have_content(job.post_name)
+  end
+
+  scenario "Show a job" do
+    visit job_path(job)
     expect(page).to have_content(job.post_name)
   end
 end

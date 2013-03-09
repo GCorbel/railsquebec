7148d34a6d0fa9c222fddb9e80e2f120037e1b0c
gcorbel <guirec.corbel@gmail.com>
2013-03-09 18:47:10 -0500

    add validations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ActiveRecord::Base
-  attr_accessible :city, :date, :description, :postal_code, :street, :title, :website
+  attr_accessible :city, :date, :description, :postal_code, :street, :title,
+                  :website
+
+  validates :title,       presence: true
+  validates :date,        presence: true
+  validates :description, presence: true
+  validates :postal_code, presence: true
+  validates :street,      presence: true
+  validates :city,        presence: true
 end

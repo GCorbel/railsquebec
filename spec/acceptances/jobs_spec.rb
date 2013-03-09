7ef1ec0bf8c24126a58834a4f284cd85a8ea78b6
gcorbel <guirec.corbel@gmail.com>
2013-03-09 18:17:03 -0500

    enable to add a new job

--- spec/acceptances/jobs_spec.rb
+++ spec/acceptances/jobs_spec.rb
@@ -12,4 +12,18 @@ feature "jobs" do
     visit job_path(job)
     expect(page).to have_content(job.post_name)
   end
+
+  scenario "Create a new job" do
+    visit new_job_path
+    fill_in "Post name", with: "Post Name"
+    fill_in "Company name", with: "Company Name"
+    fill_in "Email", with: "guirec.corbel@gmail.com"
+    fill_in "City", with: "Montreal"
+    fill_in "Street", with: "The street"
+    fill_in "Postal code", with: "A1B2C3"
+    fill_in "Description", with: "A description"
+    click_button "Submit"
+    expect(page).to have_content("Job was successfully created")
+    expect(page).to have_content("Post Name")
+  end
 end

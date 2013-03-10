7d95f4ed4964007fa3ee47db11ef7fa5729066ac
gcorbel <guirec.corbel@gmail.com>
2013-03-09 19:54:45 -0500

    add a page to show an event

--- spec/acceptances/events_spec.rb
+++ spec/acceptances/events_spec.rb
@@ -1,9 +1,15 @@
 require 'spec_helper'
 
 feature "Events" do
+  let!(:event) { create(:event) }
+
   scenario "Show the list of events" do
-    event = create(:event)
     visit events_path
+    expect(page).to have_content(event.title)
+  end
+
+  scenario "Show an event" do
+    visit event_path(event)
     expect(page).to have_content(event.title)
   end
 end

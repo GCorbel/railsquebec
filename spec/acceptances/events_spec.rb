5c1a198b88805b42814baa2ebea276efc2a6dfa1
gcorbel <guirec.corbel@gmail.com>
2013-03-09 20:14:10 -0500

    add a form to create a new event

--- spec/acceptances/events_spec.rb
+++ spec/acceptances/events_spec.rb
@@ -12,4 +12,16 @@ feature "Events" do
     visit event_path(event)
     expect(page).to have_content(event.title)
   end
+
+  scenario "Create a new event" do
+    visit new_event_path
+    fill_in "Title",       with: "Event title"
+    fill_in "Street",      with: "A street"
+    fill_in "Postal code", with: "A1B2C3"
+    fill_in "City",        with: "Montreal"
+    fill_in "Description", with: "A description"
+    click_button "Submit"
+    expect(page).to have_content("Event was successfully created")
+    expect(page).to have_content("Event title")
+  end
 end

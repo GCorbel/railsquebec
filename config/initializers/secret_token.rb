1cfe222a7d169b2b3975c10e0e7bd9afb18045b4
gcorbel <guirec.corbel@gmail.com>
2013-03-12 18:10:29 -0400

    hide the secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Railsquebec::Application.config.secret_token = '164dfb697f1b3df62c7d629b64e92ddce97b157131023173f69641a81386f2729765ebef507b2fd23e421fb727256a22fc997eed2bd3826f9b156ca45ef7d864'
+Railsquebec::Application.config.secret_token = ENV['SECRET_TOKEN']

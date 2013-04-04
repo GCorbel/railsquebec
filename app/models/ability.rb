141f7bddaa37448fd94e818fc25c470739b80296
gcorbel <guirec.corbel@gmail.com>
2013-04-04 19:26:24 -0400

    an admin can manage all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,17 @@ class Ability
     can :read, :all
 
     if user
-      can :create, :all
-      can :manage, Event do |event|
-        event.user == user
-      end
+      if user.admin?
+        can :manage, :all
+      else
+        can :create, :all
+        can :manage, Event do |event|
+          event.user == user
+        end
 
-      can :manage, Job do |job|
-        job.user == user
+        can :manage, Job do |job|
+          job.user == user
+        end
       end
     end
   end

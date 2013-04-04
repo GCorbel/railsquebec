9b0d9f6f141de0afad6e16c9a815065050873947
gcorbel <guirec.corbel@gmail.com>
2013-04-03 20:17:23 -0400

    allow to create all for a signed in user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     can :read, :all
 
     if user
+      can :create, :all
       can :manage, Event do |event|
         event.user == user
       end
